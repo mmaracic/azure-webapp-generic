fdc9825e738343b58eefd4b332eb88f9467456aa
mmaracic <marijo.maracic@gmail.com>
2023-08-20 19:33:19 +0200

    Adding axios and UI to submit data

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
-import { useState } from 'react'
+import { ChangeEvent, ClickEvent, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { TextField } from '@mui/material';
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const theme = createTheme();
+
+  const [text, setText] = useState<string>('');
+  const [response, setResponse] = useState<string>('');
+
+  const submitData = async (text: string) => {
+    try {
+      const { data, status } = await axios.post<string>(
+        'https://localhost:9000/test',
+        text,
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+      );
+      return data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log('error message: ', error.message);
+        return error.message;
+      } else {
+        console.log('unexpected error: ', error);
+        return 'An unexpected error occurred';
+      }
+    }
+  }
 
 
   return (
@@ -26,15 +52,24 @@ function App() {
         <h1>Vite + React</h1>
         <h2>Title: {import.meta.env.VITE_APP_TITLE}</h2>
         <h2>Mode: {import.meta.env.MODE}</h2>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          Increase count
-        </Button>
+        <div>
+          <TextField
+            id="outlined-multiline-static"
+            label="Message"
+            multiline
+            rows={4}
+            defaultValue={text}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => { setText(e.target.value) }}
+          />
+        </div>
+        <div>
+          <Button onClick={(e: ClickEvent<HTMLInputElement>) => { submitData(text).then((value) => setResponse(value)) }}>
+            Send to
+          </Button>
+        </div>
         <Typography variant="h5">
           <p>
-            Count is {count}
-          </p>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
+            Result is {JSON.stringify(response)}
           </p>
           <p>
             Click on the Vite and React logos to learn more
